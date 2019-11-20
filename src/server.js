f3ef73e28cada0b067ce1157c546a3e7d8fd66e9
Carlos Valdiero <carlos_valdiero@hotmail.com>
2019-11-20 16:52:04 -0300

    edited server.js and routes.js, install mongoose and dotenv, configured connection MongoBD in .env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
 
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+  
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+
+app.use(express.json());
+app.use(routes);
 
 
 app.listen(3333);
