b27eb43d39b8bf4bda4a13fc126c331014663e4e
Carlos Valdiero <carlos_valdiero@hotmail.com>
2019-11-22 15:10:52 -0300

    edited Sessioncontroller.js and model User.js, installed jsonwebtoken and bcrypt

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,11 +1,13 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports ={
 
     async store(req,res){
         const {name, email, password} = req.body;
 
-        let user = await User.findOne();
+        let user = await User.findOne({email});
 
         if(!user){
             user = await User.create({
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -16,5 +17,10 @@ const UserSchema = new mongoose.Schema({
     }
     
 }, {timestamps: true});
+
+UserSchema.pre('save',function(next){
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+});
 
 module.exports = mongoose.model("User",UserSchema);
